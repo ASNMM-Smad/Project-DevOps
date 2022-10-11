3f978c05ea8d183762c45e553d153bc99a0c9fb6
Asaf Smadja <asafsmadja@gmail.com>
2022-10-11 13:01:53 +0000

    adding and updating project files.

---
+++ Utils.py
@@ -0,0 +1,13 @@
+import time
+
+SCORES_FILE_NAME = r"C:\Users\Administrator\DevOps Expert\Scores.txt"
+BAD_RETURN_CODE = 666
+
+
+def screenCleaner():
+
+    print ("TEST", end='\r')
+    time.sleep(2)
+
+
+
