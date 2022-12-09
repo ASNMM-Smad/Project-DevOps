b97976b52d175e04565483b103f29426ca535c5a
Asaf Smadja <asafsmadja@gmail.com>
2022-12-09 13:34:16 +0200

    updating docker-compose file

--- score_service/MainScores.py
+++ score_service/MainScores.py
@@ -29,7 +29,7 @@ def score_server(status_code=0):
         </html>'''
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000, debug=True)
+    app.run(host='0.0.0.0',port=5000, debug=True)
 
 
 '''
